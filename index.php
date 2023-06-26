f85c6cb51eda0929fb50dd1ed6b0c0485d7bee5a
barney <sebphi@gmail.com>
2023-06-26 18:34:11 +0200

    Changement texte accueil + changement mot de passe + changement orientation vignettes

--- index.php
+++ index.php
@@ -42,35 +42,34 @@ function getRandomFileFromFolder($folderPath)
 </head>
 
 <body style="/*background-image:url('/img/bg/<?php echo getRandomFileFromFolder(__DIR__ . '/img/bg/') ?>');*/">
-<h1>Planning du jardin</h1>
+    <h1>Planning du jardin</h1>
     <?php
-    //echo sha1('Tallende');
-    if(isset($_GET['password']) && sha1($_GET['password'])=='1bff3dba3929e36bfce0b878bd48523317505b37'){
-        $_SESSION['user']=$_GET['user'];
+
+    if (isset($_GET['password']) && sha1($_GET['password']) == '7ec37b154f11849256633827e3670108103a79c3') {
+        $_SESSION['user'] = $_GET['user'];
     }
     if (!isset($_SESSION['user'])) {
 
         echo 'Veuillez vous connecter:';
-        echo '<form method><input type="text" name="user" value="'.($_GET['user']??'').'" placeholder="Nom d\'utilisateur"/>
-        <input type="password" name="password" value="'.($_GET['password']??'').'" placeholder="Mot de passe"/>
+        echo '<form method><input type="text" name="user" value="' . ($_GET['user'] ?? '') . '" placeholder="Nom d\'utilisateur"/>
+        <input type="password" name="password" value="' . ($_GET['password'] ?? '') . '" placeholder="Mot de passe"/>
         <input type="submit" name="submit" value="Se connecter" class="btn"/>
         </form>';
-
     } else {
 
 
     ?>
         <div class="container">
-            
-            <p>Ce site nous permet, enseignants et parents, de nous coordonner pour gérer le jardin des petits de façon collective.</p>
-            <p>Le principe est de nous aider à entretenir ce jardin, en échange de bons moments avec vos enfants et de quelques légumes à ramasser pendant l'été lorsqu'ils sont prêts! Vous pouvez ramasser tomates et courgettes, mais laissez aux élèves le plaisir de sortir les pommes de terre en septembre à la rentrée des classes. Nous les utiliserons pour la soupe d'automne.</p>
 
-            <p>Nous vous remercions de noter sur le calendrier quand vous êtes allés au jardin et ce que vous y avez fait (arrosage, désherbage, cueillette) ainsi que les jours où vous compter y aller. Vous pouvez ajouter des photos. </p>
-            <div class="toolbar">
-                <button id="calendarToogle" class="selected"><i class="fa-solid fa-calendar"></i> Voir le calendrier</button>
-                <button id="eventListButton" style="display:none"><i class="fa-solid fa-list"></i> Voir la liste des événements</button>
-                <button id="addEvent"><i class="fa-solid fa-calendar-plus"></i> Ajouter un événement</button>
-            </div>
+            <p>Chers parents, <br />Ce site nous permet, enseignants et parents, de nous coordonner pour gérer le jardin des petits de façon collective.</p>
+            <p>Le principe est de nous aider à entretenir ce jardin, en échange de bons moments avec vos enfants et de quelques légumes à ramasser pendant l'été lorsqu'ils sont prêts! Vous pouvez ramasser tomates et courgettes durant l'été, lorsque nous n'avons pas école. Laissez aux élèves le plaisir de sortir les pommes de terre, de ramasser courges, tomates et courgettes à la rentrée des classes. Nous les utiliserons pour la soupe d'automne.</p>
+
+            <p>Nous vous remercions de noter sur le calendrier quand vous êtes allés au jardin et ce que vous y avez fait (arrosage, désherbage, cueillette) ainsi que les jours où vous compter y aller. Vous pouvez ajouter des photos du jardin, de votre récolte. </p>
+            <p style="font-weight:bold;">Bon jardinage</p>
+            <button id="calendarToogle" class="selected"><i class="fa-solid fa-calendar"></i> Voir le calendrier</button>
+            <button id="eventListButton" style="display:none"><i class="fa-solid fa-list"></i> Voir la liste des événements</button>
+            <button id="addEvent"><i class="fa-solid fa-calendar-plus"></i> Ajouter un événement</button>
+        </div>
         </div>
         <div id="events-list" class="block"></div>
         <div id="calendar" class="block"></div>
